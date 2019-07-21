8807610f25c13bba0a76c15781f46f89d5db18b1
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2019-07-20 20:47:12 -0400

    Added MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const dbRoutes = require('./routes/databaseAccess.js');
 const app = express();
+let mongoose = require('mongoose');
 
 app.use('/db', dbRoutes);
 app.use(express.static('build'));
 app.listen(3000, () => {
     console.log('Server for React Todo App listening on port 3000!')
 });
+
+if (! process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+}
+
+mongoose.connection.on('connected', function() {
+    console.log('Success: connected to MongoDb!');
+});
+
+mongoose.connection.on('error', function() {
+    console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
+    process.exit(1);
+  });
+  mongoose.connect(process.env.MONGODB_URI);
